ecde3ef4f2cf0da67ee6290bcad69929e2623f15
Bonamente <56506035@protonmail.com>
2022-08-19 11:15:50 +0500

    chore: add font

---
+++ pages/_document.jsx
@@ -0,0 +1,24 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            rel="preload"
+            href="/fonts/RobotoSlab-Regular.ttf"
+            as="font"
+            crossOrigin="anonymous"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
