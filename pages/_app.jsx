9e41f236e01274a2ee14192c5bbf1c1ca0e548a3
Bonamente <56506035@protonmail.com>
2022-08-25 19:03:14 +0500

    feat: add routing at application startup

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,43 @@
-import '../styles/globals.css';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import magic from '../lib/magic-client';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+import '../styles/globals.css';
+import Loading from '../components/loading/Loading';
+
+const MyApp = ({ Component, pageProps }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleLoggedIn = async () => {
+      const isLoggedIn = await magic.user.isLoggedIn();
+
+      if (isLoggedIn) {
+        router.push('/');
+      } else {
+        router.push('/login');
+      }
+    };
+
+    handleLoggedIn();
+  }, []);
+
+  useEffect(() => {
+    const handleComplete = () => {
+      setIsLoading(false);
+    };
+
+    router.events.on('routeChangeComplete', handleComplete);
+    router.events.on('routeChangeError', handleComplete);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleComplete);
+      router.events.off('routeChangeError', handleComplete);
+    };
+  }, [router]);
+
+  return isLoading ? <Loading /> : <Component {...pageProps} />;
+};
 
 export default MyApp;
