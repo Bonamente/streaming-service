70cfc114b28d194f708e54c46085963448aa0a83
Bonamente <56506035@protonmail.com>
2022-09-07 15:05:24 +0500

    feat(CardSection): add extra rows for cards depending on prop value

--- components/card/CardSection.jsx
+++ components/card/CardSection.jsx
@@ -1,19 +1,30 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-/* eslint-disable react/no-array-index-key */
 import Link from 'next/link';
 
+import cn from 'classnames';
 import Card from './Card';
 import styles from './CardSection.module.css';
 
-const CardSection = ({ title, videos = [], size }) => (
+const CardSection = ({
+  title,
+  videos = [],
+  size,
+  shouldWrap = false,
+  shouldScale,
+}) => (
   <section className={styles.container}>
     <h2 className={styles.title}>{title}</h2>
 
-    <div className={styles.cardWrapper}>
+    <div className={cn(styles.cardWrapper, shouldWrap && styles.wrap)}>
       {videos.map((video, idx) => (
         <Link key={video.id} href={`/video/${video.id}`}>
           <a>
-            <Card key={idx} id={idx} imgUrl={video.imgUrl} size={size} />
+            <Card
+              id={idx}
+              imgUrl={video.imgUrl}
+              size={size}
+              shouldScale={shouldScale}
+            />
           </a>
         </Link>
       ))}
