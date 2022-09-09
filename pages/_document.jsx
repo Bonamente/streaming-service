12f3262d691f3eeee569d87988c62335360aaa17
Bonamente <56506035@protonmail.com>
2022-09-09 15:59:05 +0500

    feat: add extra font

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,6 +1,12 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
   render() {
     return (
       <Html lang="en">
@@ -9,18 +15,28 @@ class MyDocument extends Document {
             rel="preload"
             href="/fonts/RobotoSlab-Regular.ttf"
             as="font"
+            type="font/ttf"
             crossOrigin="anonymous"
           />
           <link
             rel="preload"
             href="/fonts/RobotoSlab-SemiBold.ttf"
             as="font"
+            type="font/ttf"
             crossOrigin="anonymous"
           />
           <link
             rel="preload"
             href="/fonts/RobotoSlab-Bold.ttf"
             as="font"
+            type="font/ttf"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/RobotoSlab-ExtraBold.ttf"
+            as="font"
+            type="font/ttf"
             crossOrigin="anonymous"
           />
         </Head>
